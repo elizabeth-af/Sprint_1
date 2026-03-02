df6b2854f5b45d26e7aafee490f6f8ef49af1ac3
elizabeth-af <liajkin@yandex.ru>
2026-03-03 00:15:23 +0300

    Доработка доп. задания 1 (task_6). Функция удаления дублей нерабочая, функция объудинения рабочая.

--- task_6.py
+++ task_6.py
@@ -13,3 +13,25 @@ tickets = {
     4: ['E2E_9', 'API_76'],
     5: ['E2E_2', 'API_61']
 }
+
+# функция удаления дублей
+# не получилось её довести до рабочего сотояния(((
+def delete(ticket):
+    for i in ticket.values():
+        for lis in i:
+            for key, value in ticket.items():
+                if lis in value:
+                    value.remove(lis)
+                    ticket[key] = value
+
+
+
+# функция объединения
+def combine(type, ticket):
+    new = {}
+    for key, value in type.items():
+        new[value] =ticket[key]
+    return new
+
+
+print(combine(types, tickets))
